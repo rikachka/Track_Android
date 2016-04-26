ab9ae4d08fae616964f44dcecf567c015ff3408a
rikachka <rikachka1@gmail.com>
2016-04-26 21:48:58 +0200

    Task2

--- app/src/main/java/com/rikachka/task1_1/MainActivity.java
+++ app/src/main/java/com/rikachka/task1_1/MainActivity.java
@@ -1,7 +1,7 @@
 package com.rikachka.task1_1;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
